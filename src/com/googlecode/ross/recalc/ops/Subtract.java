141a522d1efc00acbc34fba186323149ccbfeda4
Ross Gongaware <ross.gong@gmail.com>
2012-07-06 09:24:52 -0400

    added basic four binary operators (+,-,*,/)

--- src/com/googlecode/ross/recalc/ops/Subtract.java
+++ src/com/googlecode/ross/recalc/ops/Subtract.java
@@ -2,9 +2,9 @@ package com.googlecode.ross.recalc.ops;
 
 import com.googlecode.ross.recalc.Operator;
 
-public class Multiply extends BinaryOperator {
+public class Subtract extends BinaryOperator { 
   
   public <T extends Operator> double calc (java.util.List<T> ops) {
-    return ops.get(0).calc() * ops.get(1).calc();
+    return ops.get(0).calc() - ops.get(1).calc();
   }
 }
